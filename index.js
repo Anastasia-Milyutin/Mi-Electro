620114821b8bbf51f32834d195f6bcad6a8fc0b8
Anastasia-Milyutin <141128897+Anastasia-Milyutin@users.noreply.github.com>
2023-12-17 19:11:44 +0600

    Update index.js

--- index.js
+++ index.js
@@ -14,3 +14,47 @@ buttons.forEach((button, index) => {
     images[index].style.display = 'block';
   });
 });
+
+
+const sliderArrow = document.querySelector(".reviews__block-flex"),
+  slidesArrow = document.querySelectorAll(".block__review"),
+  left = document.querySelector(".left"),
+  right = document.querySelector(".right");
+
+let slideIndex = 0;
+
+left.addEventListener("click", () => {
+  showSlideArrow(-1);
+});
+right.addEventListener("click", () => {
+  showSlideArrow(1);
+});
+
+function showSlideArrow(n) {
+  slideIndex += n;
+
+  if (slideIndex < 0) {
+    slideIndex = slidesArrow.length - 1;
+  }
+  if (slideIndex >= slidesArrow.length) {
+    slideIndex = 0;
+  }
+
+  slidesArrow.forEach((item) => (item.style.display = "none"));
+  slidesArrow[slideIndex].style.display = "block";
+}
+
+showSlideArrow(0);
+
+
+
+const btn = document.querySelector('.review__btn');
+const modal = document.querySelector('.element-modal-window');
+const closeBtn = modal.querySelector('.klose');
+btn.addEventListener('click', () => {
+  modal.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
